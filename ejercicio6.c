af366fb4b323321af8348ffcafb7f56ac526856e
lucianocor <luboss369@gmail.com>
2024-09-03 16:28:33 -0300

    Add files via upload

---
+++ ejercicio6.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int producto();
+int main() {
+	int a,b;
+	printf("Ingresar A: ");
+	scanf("%d",&a);
+	printf("\nIngresar B: ");
+	scanf("%d",&b);
+	printf("\nEl producto  de A Y B es %d ",producto(a,b));
+	return 0;
+}
+int producto(int a,int b){
+	int sum=0;
+	do{
+		sum=sum+a;
+		b--;
+	}while(b!=0);
+	return sum;
+}
