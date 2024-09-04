8e0ce7ea0dd333f9c9305e26d2354dbeb509c295
lucianocor <luboss369@gmail.com>
2024-09-03 21:33:04 -0300

    Add files via upload

---
+++ ejercicio11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int contarmul(int x );
+int main() {
+	int x,b,cont;
+	b=0;
+	printf("Ingresar un numero X: ");
+	scanf("%d",&x);
+	while(x>0){
+		cont=contarmul(x);
+		if(cont >= x){
+			if(cont == x){
+				printf("El numero es perfecto ");
+			}else
+			   printf("El numero es abundante ");
+		}else printf("El numero es deficiente");	
+		printf("\nIngresar un numero X: ");
+		scanf("%d",&x);
+		b=1;
+	}
+	if(b==0)
+		  printf("No ingreso un numero natural");
+	return 0;
+}
+int contarmul(int a){
+	int i,contm=0;
+	i=1;
+	for(i=1;i<=a;i++){
+		if(a%i == 0)
+			contm=contm+i;}
+	return contm;
+}
---
+++ ejercicio13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int contarmul(int x);
+int main() {
+	int a,b,cont,x;
+	cont=0;
+	srand(time(NULL));
+	printf("Ingresar un nunmero A: ");
+	scanf("%d",&a);
+	printf("\nIngresar un nunmero B: ");
+	scanf("%d",&b);
+	do{
+	x=rand()%(b-a+1)+a;
+	if(contarmul(x) >= x)
+		printf("\nEl numero no es deficiente");
+	printf("%d",x);
+	cont++;
+	}while(cont<10);	
+	return 0;
+}
+int contarmul(int x){
+	int i,contm=0;
+	i=1;
+	for(i=1;i<=x;i++){
+		if(x%i == 0)
+			contm=contm+i;}
+	return contm;
+}
